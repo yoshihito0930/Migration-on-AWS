e888e9bed51c454c76082c415d0ec10d755cac9f
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2025-01-29 13:02:20 +0900

    ec2のpublic_subnet_idを更新

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,18 +1,18 @@
 module network {
-    source = "../../modules/network"
+    source                      = "../../modules/network"
 }
 
 module ec2 {
-    source = "../../modules/ec2"
-    subnet_id = module.network.public_subnet_id
+    source                      = "../../modules/ec2"
+    public_subnet_id            = module.network.public_subnet_id
 }
 
 module rds {
-    source = "../../modules/rds"
-    private_subnet_rds_id = module.network.private_subnet_rds_id
+    source                      = "../../modules/rds"
+    private_subnet_rds_id       = module.network.private_subnet_rds_id
 }
 
 module aurora {
-    source = "../../modules/aurora"
-    private_subnet_aurora_id = module.network.private_subnet_aurora_id
+    source                      = "../../modules/aurora"
+    private_subnet_aurora_id    = module.network.private_subnet_aurora_id
 }
